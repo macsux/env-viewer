25e4c824d407937e62fba4b0ee82c041f22682b0
Andrew Stakhov <andrew.stakhov@gmail.com>
2021-02-18 15:12:15 -0500

    Initial

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+
+namespace EnvironmentViewer
+{
+    public class Program
+    {
+
+
+        public static async Task Main(string[] args)
+        {
+            await Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(web => web
+                    .Configure(app => app
+                        .Run(http => http.Response.WriteAsync(JsonSerializer.Serialize(Environment.GetEnvironmentVariables())))))
+                .Build()
+                .RunAsync();
+        }
+    }
+}
+
+
+       
